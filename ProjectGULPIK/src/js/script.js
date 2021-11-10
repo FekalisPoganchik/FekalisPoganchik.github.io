17b8dcf2571790d4fffd52e8fabbe25a9af120eb
Igor <igorlebedev018@gmail.com>
2021-11-10 09:54:39 +0500

    secondfixIMG

--- ProjectGULPIK/src/js/script.js
+++ ProjectGULPIK/src/js/script.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
         // adaptiveHeight: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../image/icons/left.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../image/icons/right.png"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="image/icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="image/icons/right.png"></button>',
         responsive: [
             {
                 breakpoint: 768,
