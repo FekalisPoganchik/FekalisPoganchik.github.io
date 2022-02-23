816e2f831e94960a614954a2dc1101b2c5f408e3
Igor <igorlebedev018@gmail.com>
2022-02-23 12:47:25 +0500

    0.41CommentsJS

--- info.App.kurs/js/script.js
+++ info.App.kurs/js/script.js
@@ -46,41 +46,60 @@ routing ()
 
 
 const tasks = [];
+// Массив задач в который поступают/выводятся задачи 
+// Одна задача это один объект состаящий из полей: _id, comleted(выполнена или нет), body (текст задачи), title (заголовок задачи)
   
   (function(arrOfTasks) {
+    // Самовызывающая функция который передаются задачи
       const objOfTasks = arrOfTasks.reduce ((acc, task) => {
+        // к масиву arrOfTasks.reduce (применяет функцию к каждому элементу масива слева - направо), на каждой итерации получаем acc(аккамулятор), task(текущую задачу)
+        // Результатом будет объект объектов ключ = id значение (id comleted body title)
         acc[task._id] = task;
+        // Создаем ключ по id задачи и под ключом id поле в объекте в которую будем записывать саму задачу
         return acc;
+        // возвращаем acc
       }, {});
       // ElementsUI
-      const sectionContainer = document.querySelector('.task-progress')
-      const form = document.forms['addTask'];
-      const inputTitle = form.elements['title'];
-      const inputBody = form.elements['body'];
+      const sectionContainer = document.querySelector('.task-progress'),
+            form = document.forms['addTask'],
+            inputTitle = form.elements['title'],
+            inputBody = form.elements['body'];
 
       // Events
       renderAllTasks(objOfTasks);
+      // Вызов функции renderAllTasks(objOfTasks) (передаем в неё объект с задачей)
       form.addEventListener('submit', onFormSubmitHandler);
-      sectionContainer.addEventListener('click', onDeletehandler)
+      // Добавление события подтверждения формы
+      sectionContainer.addEventListener('click', onDeletehandler);
+      // Объект события, с обработчиком клика
 
       function renderAllTasks(tasksList) {
+        // Функция для вывода задач на страницу
+        // Для добавления в DOM менее затратно, генерируется объект с задачами, а потом весь этот объект будет добавлен в список
         if (!tasksList) {
           console.log('Отсутствует список задач');
           return;
         }
+        // If для проверка передачи объекта с задачами
 
         const fragment = document.createDocumentFragment()
+        //  Создание фрагмента для наполнения 
         Object.values(tasksList).forEach(task => {
+          // Принимает в качестве аргумента объект и возвращает его значения в виде массива для того что бы перебрать forEach на каждой его итерации мы получаем отдельную задачу 
           const section = listItemTemplate(task);
+          // Отдельная функция для создания карточки в дом объекте, равна результату вызова функции listItemTemplate передаем одну заачу.
           fragment.appendChild(section);
+          // Добавляем карточку во фрагмент 
         });
         sectionContainer.appendChild(fragment)
       }
 
       function listItemTemplate({_id, title, body} = {}) {
+        // Функция для отрисовки карточки в дом дереве получает деструктурированый task = _id, title, body, эта функция вызывается на каждой итерации 
         const section = document.createElement('div');
         section.classList.add('card', 'mb-3');
         section.setAttribute('data-task-id', _id)
+        // Вешаем атрибут с id
         
         const div = document.createElement('div');
         div.classList.add('card-body')
@@ -120,12 +139,16 @@ const tasks = [];
         wrapBtn.appendChild(deleteBtn);
 
         return section;
+        // Возвращаем для того что бы воспользоваться снаружи
       };
 
       function onFormSubmitHandler(e) {
+        // функция для отправки формы
         e.preventDefault();
         const titleValue = inputTitle.value;
+        // Получение значения Title
         const bodyValue = inputBody.value;
+        // Получение значение Body
         
         if (!titleValue || !bodyValue) {
           alert("Пожалуйста введите название и описание задачи.");
@@ -133,12 +156,17 @@ const tasks = [];
         }
         const task = createNewTask(titleValue, bodyValue);
         const listItem = listItemTemplate(task);
+        // Передаем объект в эту функцию для генерации задачи
         sectionContainer.insertAdjacentElement("beforeend", listItem)
+        // позволяет вставить элемент в одну из 4 позиций
         form.reset();
+        // Очищает форму
       }
 
       function createNewTask(title, body) {
+        // Создание задачи и добавление в DOM
         const newTask = {
+          // На основе переданных значений создается нвоый объект задачи который можно добавить в список задач
           title,
           body,
           completed: false,
@@ -151,23 +179,38 @@ const tasks = [];
       }
 
       function deleteTask(id) {
+        // Принимает id задачи котору нужно удалить так же она спрашивает точно ли нужно это сделать 
         const { title } = objOfTasks[id];
+        // Достается заголовок определнной задачи для того что бы вставить его в isConfirm
         const isConfirm = confirm(`Вы уверены что хотите удалить задачу: ${title} ?`);
+        // В переменной хранится true/false
         if (!isConfirm) return isConfirm;
-        delete objOfTasks[id];
+        // Если задача не подтверждена то не будет никаких дальнейших действий Иначе
+        delete objOfTasks[id]; 
+        // Удаление из объекта задачи под переданым id
         return isConfirm;
+        // Возващаем isConfirm для того что бы 
       }
 
       function deleteTaskFromHtml (confirmed, el) {
+        // Функция для удаления задачи из HTML разметки
         if(!confirmed) return;
+        // Если confirmed false то прекращаем работу функции иначе
         el.remove();
+        // Удаляем задачу из разметки
       }
 
       function onDeletehandler({ target }) {
+        // Обработчик удаления таски
         if(target.classList.contains('card-btn-del')) {
+          // проверка наличия класса у таргета
           const parent = target.closest('[data-task-id]');
+          // У кнопки будем искать ближайшего родителя с атрибутом data-task-id
           const id = parent.dataset.taskId;
+          // Получение id 
           const confirmed = deleteTask(id);
+          // В переменную помещается значение true/false
+
           deleteTaskFromHtml(confirmed, parent)
         }
       }
